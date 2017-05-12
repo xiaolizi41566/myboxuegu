d11ef54f7e988cbc82a874b317f0d161407fa9c3
lihao <245460681@qq.com>
2017-05-12 13:52:34 +0800

    博学谷项目结构

---
+++ js/common/main.js
@@ -0,0 +1,65 @@
+require.config({
+    baseUrl:'/',
+    paths:{
+        //common文件夹
+        aside:'js/common/aside',
+        header:'js/common/header',
+        util:'js/common/util',
+        //home文件夹下
+        index:'js/home/index',
+        login:'js/home/login',
+        repass:'js/home/repass',
+        settings:'js/home/settings',
+        //teacher文件夹
+        tcEdit:'js/teacher/edit',
+        tcList:'js/teacher/list',
+        //user文件夹
+        usProfile:'js/user/profile',
+        usList:'js/user/list',
+        //course文件夹
+        csAdd:'js/course/add',
+        csList:'js/course/list',
+        csAdd1:'js/course/course_add_step1',
+        csAdd2:'js/course/course_add_step2',
+        csAdd3:'js/course/course_add_step3',
+        cgAdd:'js/course/category_add',
+        cgList:'js/course/category_list',
+
+        //第三方模块
+        jquery:'lib/jquery/jquery.min',
+        bootstrap:'lib/bootstrap/js/bootstrap.min'
+    },
+    shim:{
+        bootstrap:{
+            deps:['jquery']
+        }
+    }
+});
+var obj ={
+    //主页面
+    '/':'index',
+    //home文件夹
+    '/html/home/login.html':'login',
+    '/html/home/repass.html':'repass',
+    '/html/home/settings.html':'settings',
+    //common文件夹
+    '/html/common/aside.html':'aside',
+    '/html/common/header.html':'header',
+    '/html/common/style.html':'style',
+    //course文件夹
+    '/html/course/add.html':'csAdd',
+    '/html/course/list.html':'csList',
+    '/html/course/category_add.html':'cgAdd',
+    '/html/course/category_list.html':'cgList',
+    '/html/course/course_add_step1.html':'csAdd1',
+    '/html/course/course_add_step2.html':'csAdd2',
+    '/html/course/course_add_step3.html':'csAdd3',
+    //teacher文件夹
+    '/html/teacher/edit.html':'tcEdit',
+    '/html/teacher/list.html':'tcList',
+    //user文件夹
+    '/html/user/list.html':'usList',
+    '/html/user/profile.html':'usProfile',
+}
+
+require([obj[location.pathname]])
---
+++ js/home/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by HP on 2017/5/12.
+ */
+define(['aside','header'], function () {
+    console.log('哈哈哈');
+})
